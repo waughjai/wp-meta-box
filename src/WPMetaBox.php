3555e6b71af8d95d332c254562ad4b59e75f9954
Jaimeson Waugh <jaimeson@4cesi.com>
2018-11-26 13:59:54 -0800

    Fix bug: missing "use" statement

--- src/WPMetaBox.php
+++ src/WPMetaBox.php
@@ -4,6 +4,7 @@ declare( strict_types = 1 );
 namespace WaughJ\WPMetaBox
 {
 	use function WaughJ\TestHashItem\TestHashItemString;
+	use function WaughJ\TestHashItem\TestHashItemExists;
 
 	class WPMetaBox
 	{
